6d33aa2a6600d17821acef21c20e396cd84664ee
Chetan2520 <148444931+Chetan2520@users.noreply.github.com>
2024-04-07 11:27:11 +0530

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>About us</li>
+      </ul>
+    </>
+  );
+}
+export default Navbar;
